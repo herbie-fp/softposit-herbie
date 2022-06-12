d8cb08ed43abf907029ae2d87733ecaeafe8ea29
Pavel Panchekha <me@pavpanchekha.com>
2022-06-12 16:51:39 -0600

    Round +inf.bf and -inf.bf to maxpos and -maxpos, not NaR

--- main.rkt
+++ main.rkt
@@ -12,27 +12,73 @@
 
 (eprintf "Loading posits support...\n")
 
+(define posit8-max (ordinal->posit8 (- (expt 2 (- 8 1)) 1)))
+(define posit16-max (ordinal->posit16 (- (expt 2 (- 16 1)) 1)))
+(define posit32-max (ordinal->posit32 (- (expt 2 (- 32 1)) 1)))
+
+;; TODO: not actually correct, it's bigger than this but hard to compute
+(define quire8-max (quire8-fdp-add (double->quire8 0.0) posit8-max posit8-max))
+(define quire8-nmax (quire8-fdp-sub (double->quire8 0.0) posit8-max posit8-max))
+(define quire16-max (quire16-fdp-add (double->quire16 0.0) posit16-max posit16-max))
+(define quire16-nmax (quire16-fdp-sub (double->quire16 0.0) posit16-max posit16-max))
+; These crash
+; (define quire32-max (quire32-fdp-add (double->quire32 0.0) posit32-max posit32-max))
+; (define quire32-nmax (quire32-fdp-sub (double->quire32 0.0) posit32-max posit32-max))
+
+(define (bf-inf->nan x) (let ([y (bf x)]) (if (bfinfinite? y) +nan.bf y)))
+
+(define (double->posit8* x)
+  (let ([y (double->posit8 x)])
+    (if (posit8= y (posit8-nar))
+        (if (> x 0) posit8-max (posit8-neg posit8-max))
+        y)))
+(define (double->posit16* x)
+  (let ([y (double->posit16 x)])
+    (if (posit16= y (posit16-nar))
+        (if (> x 0) posit16-max (posit16-neg posit16-max))
+        y)))
+(define (double->posit32* x)
+  (let ([y (double->posit32 x)])
+    (if (posit32= y (posit32-nar))
+        (if (> x 0) posit32-max (posit32-neg posit32-max))
+        y)))
+(define (double->quire8* x)
+  (let ([y (double->quire8 x)])
+    (if (infinite? (quire8->double y))
+        (if (> x 0) quire8-max quire8-nmax)
+        y)))
+(define (double->quire16* x)
+  (let ([y (double->quire16 x)])
+    (if (infinite? (quire16->double y))
+        (if (> x 0) quire16-max quire16-nmax)
+        y)))
+#;(define (double->quire32* x)
+  (let ([y (double->quire32 x)])
+    (if (infinite? (quire32->double y))
+        (if (> x 0) quire32-max quire32-nmax)
+        y)))
+
 ;; Defining the representations
 
 (define-representation (posit8 real posit8?)
-  (compose double->posit8 bigfloat->flonum)
-  (compose bf posit8->double)
+  (compose double->posit8* bigfloat->flonum)
+  (compose bf-inf->nan posit8->double)
   (shift 7 ordinal->posit8)
   (unshift 7 posit8->ordinal)
   8
   (curry posit8= (posit8-nar)))
 
 (define-representation (posit16 real posit16?)
-  (compose double->posit16 bigfloat->flonum)
-  (compose bf posit16->double)
+  (compose double->posit16* bigfloat->flonum)
+  (compose bf-inf->nan posit16->double)
   (shift 15 ordinal->posit16)
   (unshift 15 posit16->ordinal)
   16
   (curry posit16= (posit16-nar)))
 
 (define-representation (posit32 real posit32?)
-  (compose double->posit32 bigfloat->flonum)
-  (compose bf posit32->double)
+  (compose double->posit32* bigfloat->flonum)
+  (compose bf-inf->nan posit32->double)
   (shift 31 ordinal->posit32)
   (unshift 31 posit32->ordinal)
   32
@@ -40,24 +86,24 @@
 
 ;;TODO correct functions for quire (incorrect now for testing)
 (define-representation (quire8 real quire8?)
-  (compose double->quire8 bigfloat->flonum)
-  (compose bf quire8->double)
+  (compose double->quire8* bigfloat->flonum)
+  (compose bf-inf->nan quire8->double)
   (compose double->quire8 ordinal->flonum)
   (compose flonum->ordinal quire8->double)
   64
   (const #f))
 
 (define-representation (quire16 real quire16?)
-  (compose double->quire16 bigfloat->flonum)
-  (compose bf quire16->double)
+  (compose double->quire16* bigfloat->flonum)
+  (compose bf-inf->nan quire16->double)
   (compose double->quire16 ordinal->flonum)
   (compose flonum->ordinal quire16->double)
   64
   (const #f))
 
 (define-representation (quire32 real quire32?)
-  (compose double->quire32 bigfloat->flonum)
-  (compose bf quire32->double)
+  (compose double->quire32 bigfloat->flonum) ; TODO: use double->quire32* when crash fixed
+  (compose bf-inf->nan quire32->double)
   (compose double->quire32 ordinal->flonum)
   (compose flonum->ordinal quire32->double)
   64
